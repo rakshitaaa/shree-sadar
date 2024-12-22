0befcbf15aefccd5d15dc150e0d3f119a95403ca
khanNameera17 <92721716+khanNameera17@users.noreply.github.com>
2024-12-22 23:12:00 +0530

    put social links and make small changes

--- js/map.js
+++ js/map.js
@@ -1,7 +1,29 @@
+// function initialize() {
+// 	var myOptions = {
+// 		zoom: 15,
+// 		center: new google.maps.LatLng(51.488966, -0.096777), //change the coordinates
+// 		mapTypeId: google.maps.MapTypeId.ROADMAP,
+// 		scrollwheel: false,
+// 		mapTypeControl: false,
+// 		zoomControl: false,
+// 		streetViewControl: false
+// 	};
+// 	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+// 	var marker = new google.maps.Marker({
+// 		map: map,
+// 		position: new google.maps.LatLng(51.488966, -0.096777) //change the coordinates
+// 	});
+// 	google.maps.event.addListener(marker, "click", function() {
+// 		infowindow.open(map, marker);
+// 	});
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
+
+
 function initialize() {
 	var myOptions = {
 		zoom: 15,
-		center: new google.maps.LatLng(51.488966, -0.096777), //change the coordinates
+		center: new google.maps.LatLng(26.912491, 75.787210), //change the coordinates
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		scrollwheel: false,
 		mapTypeControl: false,
@@ -11,10 +33,23 @@ function initialize() {
 	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 	var marker = new google.maps.Marker({
 		map: map,
-		position: new google.maps.LatLng(51.488966, -0.096777) //change the coordinates
+		position: new google.maps.LatLng(26.912491, 75.787210) //change the coordinates
 	});
+ var infowindow = new google.maps.InfoWindow({
+  content: '<p>Anantpura Main Rd, Rampura Dabri, Rajasthan 303704</p>'
+ });
+
+ google.maps.event.addListener(marker , "mouseover",function(){
+  infowindow.open(map,marker)
+ });
+
+ google.maps.event.addListener(marker , "mouseout",function(){
+  infowindow.close()
+ });
+ 
 	google.maps.event.addListener(marker, "click", function() {
-		infowindow.open(map, marker);
+  window.open("https://www.google.com/maps?q=Shree Sadar Machines, 4PFV+567, Anantpura Main Rd, Jaitpura, Jaipur, Rajasthan 303704", "_blank"); //to open map with current address
+
 	});
 }
 google.maps.event.addDomListener(window, 'load', initialize);
